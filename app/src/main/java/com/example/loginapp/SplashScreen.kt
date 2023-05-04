7073d8d9c9477cc1d7b40fcf4d529ac3ea20b0f8
Akshay@2003 <akshay88magar@gmail.com>
2023-05-04 16:57:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/loginapp/SplashScreen.kt
@@ -0,0 +1,20 @@
+package com.example.loginapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.core.os.postDelayed
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        supportActionBar?.hide()
+        Handler(Looper.getMainLooper()).postDelayed(2000){
+            startActivity(Intent(this,LoggedIn::class.java))
+            finish()
+        }
+    }
+}
